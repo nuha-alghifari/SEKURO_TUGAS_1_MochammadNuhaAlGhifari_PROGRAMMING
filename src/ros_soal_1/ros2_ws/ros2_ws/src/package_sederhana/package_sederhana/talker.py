13edcade530f36223f6811141b30fdf8a002d009
Nuha <nuhaa.alghifari@gmail.com>
2026-03-25 15:53:31 +0700

    feat: selesaikan soal ROS 2

---
+++ src/ros_soal_1/ros2_ws/ros2_ws/src/package_sederhana/package_sederhana/talker.py
@@ -0,0 +1,27 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class Talker(Node):
+    def __init__(self):
+        super().__init__('talker_node')
+        self.pub = self.create_publisher(String, 'chatter', 10)
+        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.count = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = f'Halo Sekuro: {self.count}'
+        self.pub.publish(msg)
+        self.get_logger().info(f'Publishing: "{msg.data}"')
+        self.count += 1
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Talker()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
